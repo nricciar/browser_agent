c4d7f0635b5adb3e16fcecafd74284ea4a268a88
David Ricciardi <nricciar@gmail.com>
2013-05-24 15:41:49 -0400

    rails client

--- lib/browser_agent/rails_client.rb
+++ lib/browser_agent/rails_client.rb
@@ -6,23 +6,35 @@ module BrowserAgent
     def fetch(uri, *args)
       default_options = { :method => :get, :limit => 10, :referer => nil, :debug => false }
       options = default_options.merge(args.first)
+      if uri =~ /^\// && !@domain.nil?
+        uri = File.join("#{@scheme}://",@domain,uri)
+      end
       @current_location = uri
+
+      headers = {}
+      headers['User-Agent'] = USER_AGENT
+      headers['Cookie'] = @cookie_jar.get_cookie_header(uri)
+
+      puts "#{options[:method].to_s.upcase} #{uri} -- #{headers.inspect}"
 
       route = Rails.application.routes.recognize_path(path, :method => options[:method])
       env = Rack::MockRequest.env_for(uri, :params => route, 'HTTP_HOST' => @domain)
       controllerClass = "#{route[:controller]}_controller".camelize.constantize
       endpoint = controllerClass.action(route[:action])
-      @status, headers, body = endpoint.call(env)
+      @status, headers, response = endpoint.call(env)
+
+      @current_location = headers['location'] || uri
+      @cookie_jar.set_cookies_from_headers(@current_location, headers)
+      @response = response.body
+      @scheme = response.request.env["rack.url_scheme"]
 
       case @status
       when 200
-      when 302
+        @document = HtmlDocument.new(response.body, self)
+      when 301..302
         loc = headers["Location"]
         fetch(loc, :limit => (options[:limit] - 1), :referer => uri)
-      when 301
       end
-      puts "#{@status.inspect} -- #{headers.inspect}"
-      puts "#{uri} -- #{args.inspect}"
     end
 
   end
